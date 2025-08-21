508056a0dd3cac0f67c51036e1eabb7fabe55847
Sabari Raj P <143380285+sabarirajpazhani@users.noreply.github.com>
2025-08-21 23:07:58 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using DB_First.Models;
+
+namespace DB_First
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (EntityFrameWorkCoreContext context = new EntityFrameWorkCoreContext())
+            {
+                //GET ALL
+                Console.WriteLine("------------------- GET ALL --------------------");
+                var customer = context.Customers.Select(a => a);
+                foreach (var c in customer)
+                {
+                    Console.WriteLine(c.CustomerName);
+                }
+                Console.WriteLine();
+
+                //GET BY ID
+                Console.WriteLine("------------------ GET BY ID ------------------");
+                var customerByID = context.Customers.Find(100);
+                Console.WriteLine(customerByID.CustomerName);
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+
+
+
